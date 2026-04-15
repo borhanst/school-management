44b0d37e46cba515cc8a0e7bde35b5d77a86c5d3
borhanst <mdborhan.st@gmail.com>
2026-04-15 11:15:42 +0600

    add pg database

--- school_management/settings.py
+++ school_management/settings.py
@@ -95,10 +95,19 @@ WSGI_APPLICATION = "school_management.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
+    # "default": {
+    #     "ENGINE": "django.db.backends.sqlite3",
+    #     "NAME": BASE_DIR / "db.sqlite3",
+    # }
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("DB_NAME", "school_management"),
+        "USER": os.environ.get("DB_USER", "postgres"),
+        "PASSWORD": os.environ.get("DB_PASSWORD", "postgres"),
+        "HOST": os.environ.get("DB_HOST", "localhost"),
+        "PORT": os.environ.get("DB_PORT", "5432"),
+    }     
+    
 }
 
 # Password validation
